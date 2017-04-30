e9b957e536f7ff6aa95486e449ef4e64f79961e0
jsf395 <jsf395@nyu.edu>
2017-04-30 10:37:03 -0400

    Add files via upload

---
+++ insertLoc.php
@@ -0,0 +1,111 @@
+<html>
+<head>
+<style>
+span.ch{
+	color:red;
+}
+</style>
+</head>
+<body>
+<?php
+$loc = htmlspecialchars(strip_tags(trim($_GET['Locname'])));
+echo $loc;
+echo "<br>";
+$street = htmlspecialchars(strip_tags(trim($_GET['Stname'])));
+echo $street;
+echo "<br>";
+$city = htmlspecialchars(strip_tags(trim($_GET['Ctname'])));
+echo $city;
+echo "<br>";
+$zip = htmlspecialchars(strip_tags(trim($_GET['Zname'])));
+echo $zip;
+echo "<br>";
+$Lat = htmlspecialchars(strip_tags(trim($_GET['Latname'])));
+echo $Lat;
+echo "<br>";
+$Long = htmlspecialchars(strip_tags(trim($_GET['Lngname'])));
+echo $Long;
+echo "<br>";
+$Desc = htmlspecialchars(strip_tags(trim($_GET['Desc'])));
+echo $Desc;
+echo "<br>";
+
+$check = False;
+if(!isset($loc) || trim($loc) == ''){
+	echo '<span class="ch">Missing Location Name</span> <br>';
+	$check = False;
+}
+else{
+	$check = True;
+}
+if(!isset($street) || trim($street) == ''){
+	echo '<span class="ch">Missing Street Name</span> <br>';
+	$check = False;
+}
+else{
+	$check = True;
+}
+if(!isset($city) || trim($city) == ''){
+	echo '<span class="ch">Missing City Name</span> <br>';
+	$check = False;
+}
+else{
+	$check = True;
+}
+if(!isset($zip) || trim($zip) == ''){
+	echo '<span class="ch">Missing Zipcode</span> <br>';
+	$check = False;
+}
+else{
+	$check = True;
+}
+if(!isset($Lat) || trim($Lat) == '' || filter_var($Lat,FILTER_VALIDATE_FLOAT) === False){
+	echo '<span class="ch">Missing Latitude or not a decimal</span> <br>';
+	$check = False;
+}
+else{
+	$check = True;
+}
+if(!isset($Long) || trim($Long) == '' || filter_var($Long,FILTER_VALIDATE_FLOAT) === False){
+	echo '<span class="ch">Missing Longitude or not a decimal</span> <br>';
+	$check = False;
+}
+else{
+	$check = True;
+}
+if(!isset($Desc) || trim($Desc) == ''){
+	echo '<span class="ch">Missing Description</span> <br>';
+	$check = False;
+}
+else{
+	$check = True;
+}
+$inLoc="'".$loc."'";
+$inZip=(int)$zip;
+$inStreet="'".$street."'";
+$inCity="'".$city."'";
+$inDesc="'".$Desc."'";
+$inLat=(float)$Lat;
+$inLong=(float)$Long;
+if($check == True){
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "cs3083_proj";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	}
+	$sql ="INSERT INTO Location VALUES($inLoc,$inZip,$inStreet,$inCity,$inDesc,$inLat,$inLong)";
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+}
+?>
+</body>
+</html>
---
+++ makeLoc.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 50%;
+		width:50%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+	  input.inp{
+	width:20%;
+	
+}
+input.din {
+    line-height: 100px;
+	width:30%;
+}
+    </style>
+	<script type="text/javascript" src="jsEvents.js"></script>
+  </head>
+  <body>
+  <div id="floating-panel">
+      <input id="address" type="textbox" placeholder="Location Name,Street,City">
+      <input id="submit" type="button" value="Geocode">
+    </div>
+    <div id="map"></div>
+    <script>
+      var map;
+	  var myLatLng = {lat: 40.363, lng: -73.044};
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.6782, lng: -73.9422},
+          zoom: 8
+        });
+		
+        google.maps.event.addListener(map,'click',function(event) {
+          document.getElementById('Lat').value = event.latLng.lat()
+          document.getElementById('Lng').value = event.latLng.lng()
+		  document.getElementById("latlng").value = event.latLng.lat()+","+event.latLng.lng();
+        });
+		 var geocoder = new google.maps.Geocoder;
+		var infowindow = new google.maps.InfoWindow;
+		 document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+		  
+        });
+		
+      }
+	  
+	  function geocodeLatLng(geocoder, map, infowindow) {
+        var inputLat = document.getElementById('Lat').value;
+		var inputLong =document.getElementById('Lng').value;
+        var latlng = {lat: parseFloat(inputLat), lng: parseFloat(inputLong)};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              map.setZoom(11);
+              var marker = new google.maps.Marker({
+                position: latlng,
+                map: map
+              });
+              infowindow.setContent(results[1].formatted_address);
+              infowindow.open(map, marker);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+      }
+	
+	  function updateLoc(){
+	  document.getElementById('address').value = document.getElementById('LName').value+','+document.getElementById('Street').value+','+document.getElementById('City').value+","+document.getElementById("Zip").value;
+	 }
+	  function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('address').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+			var latitude = results[0].geometry.location.lat();
+			var longitude = results[0].geometry.location.lng();
+			 document.getElementById('Lat').value = latitude;
+          document.getElementById('Lng').value = longitude;
+		  
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+		
+		 
+			
+	
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCT1GVFMs6VUuEqfTiT9IHelucIxidZRoA&callback=initMap"
+    async defer></script>
+	<br>
+	<form action="insertLoc.php" method="get">
+	<span>Location Name:</span><input class="inn" id="LName" name="Locname" maxlength="20" onkeyup="updateLoc()">
+	<span>Street:<span><input class="inn" id="Street" name="Stname" maxlength="50" onkeyup="updateLoc()">
+	<span>City:</span><input class="inn" id="City" name="Ctname" maxlength="20" onkeyup="updateLoc()">
+	<br>
+	<br>
+	<span>Zip:</span><input class="inn" type="text" name="Zname" id="Zip" value="00000" maxlength="5" onkeyup="updateLoc()" onkeypress="return isNumberKey(event)">
+	<span>Latitude:</span><input class="inn" name="Latname" maxlength="50" id="Lat">
+	<span>Longitude:</span><input class="inn" name="Lngname" maxlength="50" id="Lng">
+	<br>
+	<br>
+	<!-- <span>Description:</span><input type="text" class="din" name="Desc" id="Descript">  -->
+  <label for="Desc"> Description: </label>
+  <textarea name="Desc" class="din" id="Descript" rows="10" cols="40"></textarea>
+		<br>
+	<input type="submit">
+	<br>
+	</form>
+  </body>
+</html>
