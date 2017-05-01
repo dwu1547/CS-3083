a93baf780a602c7f48f1ff0a64d4636cdcb6f73d
Home - Josh <jsf395@nyu.edu>
2017-04-30 21:38:20 -0400

    added view groups by interest

---
+++ interestGetGroups.php
@@ -0,0 +1,42 @@
+<!DOCTYPE>
+<html>
+<head>
+</head>
+<body>
+<div>SELECT INTEREST TO GET GROUPS</div>
+<br>
+<?php
+$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "meetup";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	$sql = "SELECT * from interest";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+    // output data of each row
+	echo "<form action='interestShowGroups.php' method='get'>";
+	echo "<select name='chooseInt'>";
+		while($row = mysqli_fetch_array($result))
+		{
+		//echo "<br>", $row['group_id'],"</br>";
+			echo "<option>".$row['interest_name']."</option>";
+		} 
+	echo "</select>";
+	echo "<input type='submit'>";
+	echo "</form>";
+	}
+	
+	else {
+    echo "0 results";
+	}
+	$conn->close();
+?>
+</body>
+</html>
---
+++ interestShowGroups.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+$int = htmlspecialchars(strip_tags(trim($_GET['chooseInt'])));
+//echo $int;
+$inpINT = "'".$int."'";
+$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "meetup";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	$sql = "SELECT groups.group_id,groups.group_name,groups.description,groups.username from groups,about
+	where groups.group_id = about.group_id AND about.interest_name = $inpINT";
+	$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+echo "<table>";
+echo "<tr><td>","Group_id","</td><td>","Group_Name","</td><td>","Description","</td><td>","Creator","</td><tr>";
+	while($row =mysqli_fetch_assoc($result)){
+		echo"<tr><td>",$row['group_id'],"</td><td>".$row['group_name'],"</td><td>",$row['description'],
+		"</td><td>",$row['username'],"</td></tr>";
+	}
+echo"</table>";
+}
+else {
+echo "0 results";
+}
+$conn->close();
+?>
+</body>
+</html>
